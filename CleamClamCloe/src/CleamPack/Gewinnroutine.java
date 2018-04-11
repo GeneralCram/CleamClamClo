1e3d33031731152cb024e4d6428f1871b58cf071
cram <cram@192.168.0.101>
2018-04-11 17:19:59 +0200

    Der Debugger!

--- CleamClamCloe/src/CleamPack/Gewinnroutine.java
+++ CleamClamCloe/src/CleamPack/Gewinnroutine.java
@@ -27,7 +27,7 @@ public class Gewinnroutine {
 					if (GUI.state[6] == 1 && GUI.state[7] == 1 && GUI.state[8] == 1) {
 						GUI.gewinner = 1;
 					}
-
+					
 					// Reihe 1 für X Vertikal
 					if (GUI.state[0] == 1 && GUI.state[3] == 1 && GUI.state[6] == 1) {
 						GUI.gewinner = 1;
@@ -49,41 +49,40 @@ public class Gewinnroutine {
 						GUI.gewinner = 1;
 					}
 				}
-				
+			
 				
 				if (GUI.gewinner == 0) {
-
+					
 					// Reihe 1 für O Horizontal
-					if (GUI.state[0] == 2 && GUI.state[1] == 1 && GUI.state[2] == 1) {
+					if (GUI.state[0] == 2 && GUI.state[1] == 2 && GUI.state[2] == 2) {
 						GUI.gewinner = 2;
 					}
 					// Reihe 2 für O Horizontal
-					if (GUI.state[3] == 2 && GUI.state[4] == 1 && GUI.state[5] == 1) {
+					if (GUI.state[3] == 2 && GUI.state[4] == 2 && GUI.state[5] == 2) {
 						GUI.gewinner = 2;
 					}
 					// Reihe 3 für O Horizontal
-					if (GUI.state[6] == 2 && GUI.state[7] == 1 && GUI.state[8] == 1) {
+					if (GUI.state[6] == 2 && GUI.state[7] == 2 && GUI.state[8] == 2) {
 						GUI.gewinner = 2;
 					}
-
 					// Reihe 1 für O Vertikal
-					if (GUI.state[0] == 2 && GUI.state[3] == 1 && GUI.state[6] == 1) {
+					if (GUI.state[0] == 2 && GUI.state[3] == 2 && GUI.state[6] == 2) {
 						GUI.gewinner = 2;
 					}
 					// Reihe 2 für O Vertikal
-					if (GUI.state[1] == 2 && GUI.state[4] == 1 && GUI.state[7] == 1) {
+					if (GUI.state[1] == 2 && GUI.state[4] == 2 && GUI.state[7] == 2) {
 						GUI.gewinner = 2;
 					}
 					// Reihe 3 für O Vertikal
-					if (GUI.state[2] == 2 && GUI.state[5] == 1 && GUI.state[8] == 1) {
+					if (GUI.state[2] == 2 && GUI.state[5] == 2 && GUI.state[8] == 2) {
 						GUI.gewinner = 2;
 					}
 					// Diagonal links oben nach rechts unten für O
-					if (GUI.state[0] == 2 && GUI.state[4] == 1 && GUI.state[8] == 1) {
+					if (GUI.state[0] == 2 && GUI.state[4] == 2 && GUI.state[8] == 2) {
 						GUI.gewinner = 2;
 					}
 					// Diagonal rechts oben nach links unten für O
-					if (GUI.state[2] == 2 && GUI.state[4] == 1 && GUI.state[6] == 1) {
+					if (GUI.state[2] == 2 && GUI.state[4] == 2 && GUI.state[6] == 2) {
 						GUI.gewinner = 2;
 					}
 				}
